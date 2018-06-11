6cf43fcd3bf0840d46f4a5c3fe6e0b76c261a21b
GeovanaS <geovanasilvadasilveira1@gmail.com>
2018-06-11 23:03:56 +0000

    atualizado com o calculo do erro

--- mlp.py
+++ mlp.py
@@ -10,9 +10,12 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.datasets import load_breast_cancer
 # importa modelo train_test_split para realizar a divisao do dataset em conjuntos de treino e teste
 from sklearn.model_selection import train_test_split 
-# importa metrica para o calculo do erro medio quadratico
-from sklearn.metrics import mean_squared_error
-
+# importa classe StandardScaler para realizar a normalização dos dados
+from sklearn.preprocessing import StandardScaler
+# importa biblioteca numpy para o calculo do erro medio quadratico
+import numpy as np
+# importa biblioteca matplotlib para a criacao do grafico
+import matplotlib.pyplot as plt
 
 # carrega o dataset
 dados = load_breast_cancer()
@@ -26,12 +29,23 @@ y = dados['target'] # armazena o mapeamento para binario das classificacao dos c
 # divide os dados para treino e teste utilizando uma porcentagem de 34% para teste e 66% para treinamento
 x_treino,x_teste,y_treino,y_teste = train_test_split(x, y, test_size = 0.34)
 
+escala = StandardScaler()
+
+# treina os dados normalizados
+escala.fit(x_treino)
+
+x_teste = escala.transform(x_teste)
+x_treino = escala.transform(x_treino)
+
 # inicializa a classificacao passando uma tupla com numero de neuronios em cada camada e o numero maximo de iteracoes/epocas 
-mlp = MLPClassifier(hidden_layer_sizes = (20,20,20), max_iter = 100)
+mlp = MLPClassifier(hidden_layer_sizes = (20,20,20), max_iter = 500)
 
 # treina a classificacao
 mlp.fit(x_treino, y_treino)
 
+# realiza a previsao
+previsao = mlp.predict(x_teste)
 
-
-
+# calcula erro medio quadratico
+emq = np.mean((mlp.predict(x_teste) - y_teste) ** 2)
+print("Erro Medio:\n", emq)
