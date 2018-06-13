1bc5c666f221aa30fe9b2905ed816fe5136c8554
GeovanaS <geovanasilvadasilveira1@gmail.com>
2018-06-13 01:33:30 +0000

    trabalho atualizado

--- mlp.py
+++ mlp.py
@@ -1,8 +1,8 @@
-######################################################################################################################################################## 
-#  Trabalho FIA - Rede Neural Artificial                                                           				    			    				   #
-#  Redes Neurais Multiplayer Perception capaz de classificar corretamente casos de cancer de mama  				    			    				   #
-#  Grupo: Gabriella Selbach, Geovana Silveira, Luiza Cruz                                          				    			    				   #
-########################################################################################################################################################
+#################################################################################################################### 
+#  Trabalho FIA - Rede Neural Artificial                                                           			  	   #
+#  Redes Neurais Multiplayer Perception capaz de classificar corretamente casos de cancer de mama  				   #
+#  Grupo: Gabriella Selbach, Geovana Silveira, Luiza Cruz                                          				   #
+####################################################################################################################
 
 # importa classe MLPClassifier que implementa o algoritmo de multiplayer perceptron que realiza o treinamento usando backpropagation
 from sklearn.neural_network import MLPClassifier
@@ -12,10 +12,10 @@ from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split 
 # importa classe StandardScaler para realizar a normalização dos dados
 from sklearn.preprocessing import StandardScaler
-# importa biblioteca numpy para o calculo do erro medio quadratico
-import numpy as np
-# importa biblioteca matplotlib para a criacao do grafico
-import matplotlib.pyplot as plt
+# importa metrica para a criacao da matriz de confusao e  para o calculo do erro medio quadratico
+from sklearn.metrics import confusion_matrix,mean_squared_error
+# importa biblioteca pandas para utilizacao do metodo crosstab
+import pandas as pd
 
 # carrega o dataset
 dados = load_breast_cancer()
@@ -38,7 +38,7 @@ x_teste = escala.transform(x_teste)
 x_treino = escala.transform(x_treino)
 
 # inicializa a classificacao passando uma tupla com numero de neuronios em cada camada e o numero maximo de iteracoes/epocas 
-mlp = MLPClassifier(hidden_layer_sizes = (20,20,20), max_iter = 500)
+mlp = MLPClassifier(hidden_layer_sizes = (20,20,20), max_iter = 300)
 
 # treina a classificacao
 mlp.fit(x_treino, y_treino)
@@ -47,5 +47,10 @@ mlp.fit(x_treino, y_treino)
 previsao = mlp.predict(x_teste)
 
 # calcula erro medio quadratico
-emq = np.mean((mlp.predict(x_teste) - y_teste) ** 2)
+emq = mean_squared_error(y_teste,mlp.predict(x_teste))
 print("Erro Medio:\n", emq)
+
+#imprime crosstab
+print(pd.crosstab(y_teste,mlp.predict(x_teste),rownames=['Real'],colnames=['Predito'],margins=True))
+#imprime matriz de confusao
+#print(confusion_matrix(y_teste,mlp.predict(x_teste)))
